f267026592d047d503b3c10ea55d4951bc90fcc2
Sonal Ramnathkar <sonal.ramnathkar@gmail.com>
2013-07-11 19:51:07 +0000

    correction in grader

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-77;10103;0c#!/usr/bin/env node
+#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
